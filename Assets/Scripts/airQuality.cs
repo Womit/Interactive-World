c4d41edc92ae4c95b54826f82ca709813aa0147b
Ryan Brereton <ryan.brereton@outlook.com>
2022-04-26 11:33:13 +0100

    scripted slider

--- Assets/Scripts/airQuality.cs
+++ Assets/Scripts/airQuality.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class airQuality : MonoBehaviour
 {
@@ -9,20 +10,29 @@ public class airQuality : MonoBehaviour
     private float minValue;
     private float maxValue;
 
-    public void submitSliderSetting() {
-        Debug.Log(mainSlider.value);
-    }
-    public airQuality mainSlider;
+    private Slider slider;
+    public GameObject mainSlider;
+
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
-        mainSlider.minValue = 1;
-        mainSlider.maxValue = 10;
+        
+       slider = mainSlider.GetComponent<Slider>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject Cube = GameObject.FindGameObjectWithTag("Pollution");
+
+        float dist = Vector3.Distance(player.transform.position, Cube.transform.position);
+        Debug.Log("Distance to other: " + dist);
+
+
+        slider.value = dist;
     }
 }
